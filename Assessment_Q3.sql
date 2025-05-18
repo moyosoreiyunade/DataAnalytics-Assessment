1e35cc33a6d24f47aaf01f9ec2c9a166e5f28ac8
Moyosore Iyunade <153443762+moyosoreiyunade@users.noreply.github.com>
2025-05-18 10:23:33 +0100

    Update Assessment_Q3.sql

--- Assessment_Q3.sql
+++ Assessment_Q3.sql
@@ -1,10 +1,10 @@
 -- Find all active accounts (savings or investments) with no transactions in the last 1 year (365 days).
 
 SELECT a.id AS plan_id, a.owner_id, CASE 
-										 WHEN a.is_a_fund = 1 THEN 'Investments'
-										 WHEN a.is_regular_savings = 1 THEN 'Savings'
-										 ELSE 'N/A' 
-									END AS type, -- When is_a_fund equals 1, return Investments. When is_regular_savings equals 1, return Savings
+					 WHEN a.is_a_fund = 1 THEN 'Investments'
+					 WHEN a.is_regular_savings = 1 THEN 'Savings'
+					 ELSE 'N/A' 
+				END AS type, -- When is_a_fund equals 1, return Investments. When is_regular_savings equals 1, return Savings
 	   MAX(b.transaction_date) AS last_transaction_date, DATEDIFF(CURDATE(), MAX(b.transaction_date)) as inactivity_days -- duration in days between current date and the last_transaction_date
 FROM adashi_staging.plans_plan a
 LEFT JOIN adashi_staging.savings_savingsaccount b
